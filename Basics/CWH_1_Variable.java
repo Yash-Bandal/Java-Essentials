9e586eb58129664d0e78ee0680669d91617c16ff
Yash L <147707204+YashL3616@users.noreply.github.com>
2024-04-07 21:48:32 +0530

    Create CWH_1_Variable.java

---
+++ Basics/CWH_1_Variable.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Variable {
+    public static void main(String[] args) {
+        System.out.println("Sum calculation");
+        int var1=10;
+        int var2=20;
+        int var3=30;
+        int sum=var1+var2+var3;
+        System.out.println("The sume is "+sum);
+    }
+}
