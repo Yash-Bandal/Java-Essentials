d858296bb4895f5752a9dd3b681a27061514084a
Yash L <147707204+YashL3616@users.noreply.github.com>
2024-04-09 23:14:07 +0530

    Create StringDemo.java

---
+++ String/StringDemo.java
@@ -0,0 +1,9 @@
+package com.company;
+import java.lang.String;
+public class StringDemo {
+    public static void main(String[] args) {
+        String name=new String("Yash");
+        //String name="Yash";
+        System.out.println(name);
+    }
+} 
