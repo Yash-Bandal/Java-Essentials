fe9f2ec4f4e0204310c0855aef9ca852de6a5fe2
Yash L <147707204+YashL3616@users.noreply.github.com>
2024-04-11 17:24:05 +0530

    Create PrintMethods.java

---
+++ String/PrintMethods.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class PrintMethods {
+    public static void main(String[] args) {
+
+        int a=10;
+        System.out.println("Prints new Line");
+
+        System.out.print("No New line..conti");
+        System.out.print("nue");
+
+        System.out.println("\n");
+
+
+        System.out.printf("Hello C world!\n");
+        System.out.printf("%d",a);
+    }
+}
